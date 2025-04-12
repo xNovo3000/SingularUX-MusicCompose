c386b2b8a8deea21332e267f8febde654d65e11a
xNovo3000 <fontana.ciro.2001@gmail.com>
2025-04-12 20:15:42 +0200

    Finished first part of SearchBar component

--- feature/home/src/main/kotlin/org/singularux/music/feature/home/ui/SearchBarComponent.kt
+++ feature/home/src/main/kotlin/org/singularux/music/feature/home/ui/SearchBarComponent.kt
@@ -1,7 +1,12 @@
 package org.singularux.music.feature.home.ui
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.ArrowBack
+import androidx.compose.material.icons.rounded.Clear
+import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -13,8 +18,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.isTraversalGroup
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.traversalIndex
 import androidx.compose.ui.tooling.preview.Preview
 import org.singularux.music.core.ui.MusicSurface
 import org.singularux.music.core.ui.MusicTheme
@@ -22,7 +31,7 @@ import org.singularux.music.feature.home.R
 
 @ExperimentalMaterial3Api
 @Composable
-fun SearchBarInputField(
+fun SearchBarComponent(
     modifier: Modifier = Modifier,
     query: String,
     onQueryChange: (String) -> Unit,
@@ -30,32 +39,54 @@ fun SearchBarInputField(
     expanded: Boolean,
     onExpandedChange: (Boolean) -> Unit
 ) {
-    SearchBarDefaults.InputField(
+    SearchBar(
         modifier = modifier,
-        query = query,
-        onQueryChange = onQueryChange,
-        onSearch = onSearch,
-        expanded = expanded,
-        onExpandedChange = onExpandedChange,
-        leadingIcon = {
-
-        },
-        trailingIcon = {
-            if (expanded) {
-                IconButton(
-                    onClick = { onQueryChange("") }  // Clear "query"
-                ) {
-
+        inputField = {
+            SearchBarDefaults.InputField(
+                query = query,
+                onQueryChange = onQueryChange,
+                onSearch = onSearch,
+                expanded = expanded,
+                onExpandedChange = onExpandedChange,
+                placeholder = {
+                    Text(text = stringResource(R.string.search_bar_placeholder))
+                },
+                leadingIcon = {
+                    if (expanded) {
+                        IconButton(
+                            onClick = { onExpandedChange(false) }  // Collapse
+                        ) {
+                            Icon(
+                                imageVector = Icons.AutoMirrored.Rounded.ArrowBack,
+                                contentDescription = stringResource(R.string.search_bar_back)
+                            )
+                        }
+                    } else {
+                        Icon(
+                            imageVector = Icons.Rounded.Search,
+                            contentDescription = stringResource(R.string.search_bar_search)
+                        )
+                    }
+                },
+                trailingIcon = {
+                    if (expanded) {
+                        IconButton(
+                            onClick = { onQueryChange("") }  // Clear "query"
+                        ) {
+                            Icon(
+                                imageVector = Icons.Rounded.Clear,
+                                contentDescription = stringResource(R.string.search_bar_clear_all)
+                            )
+                        }
+                    }
                 }
-            }
-        }
-    )
-}
-
-@ExperimentalMaterial3Api
-@Composable
-fun SearchBarComponent(modifier: Modifier = Modifier) {
-
+            )
+        },
+        expanded = expanded,
+        onExpandedChange = onExpandedChange
+    ) {
+        // TODO: Add content
+    }
 }
 
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_NO)
@@ -65,7 +96,24 @@ fun SearchBarComponent(modifier: Modifier = Modifier) {
 private fun Preview() {
     MusicTheme {
         MusicSurface {
-
+            var expanded by rememberSaveable { mutableStateOf(false) }
+            var query by rememberSaveable { mutableStateOf("") }
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .semantics { isTraversalGroup = true }
+            ) {
+                SearchBarComponent(
+                    modifier = Modifier
+                        .align(Alignment.TopCenter)
+                        .semantics { traversalIndex = 0F },
+                    query = query,
+                    onQueryChange = { query = it },
+                    onSearch = {},
+                    expanded = expanded,
+                    onExpandedChange = { expanded = it }
+                )
+            }
         }
     }
 }
